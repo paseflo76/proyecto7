e0328c30c926f8203c34a65973290386b93957d5
Pablo Serrano <160110487+paseflo76@users.noreply.github.com>
2025-02-13 10:07:39 +0100

    Update user.js

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -1,10 +1,18 @@
 const { isAdmin } = require('../../middlewares/auth')
-const { getUsers, register, login, deleteUser } = require('../controller/user')
+const {
+  getUsers,
+  register,
+  login,
+  deleteUser,
+  updateUser
+} = require('../controller/user')
 
 const usersRoutes = require('express').Router()
 
 usersRoutes.get('/', [isAdmin], getUsers)
 usersRoutes.post('/register', register)
 usersRoutes.post('/login', login)
+usersRoutes.put('/:id', [isAdmin], updateUser)
 usersRoutes.delete('/:id', [isAdmin], deleteUser)
+
 module.exports = usersRoutes
